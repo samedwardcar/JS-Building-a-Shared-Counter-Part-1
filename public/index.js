721e19f8ee1cfd919632f4607595bf067e764ec2
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2023-07-15 16:38:36 -0500

    finished code with bonus

--- public/index.js
+++ public/index.js
@@ -1,22 +1,36 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button');
 
-    let countValue = 0;
+    const response = await fetch('http://localhost:9001/counter');
+
+    const result = await response.json();
+
+    let countValue = result.value;
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        // console.log(countValue);
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        // console.log(countValue);
+    }
+
+    function refresh(){
+        const countValue = result.value;
+        countContainer.textContent =countValue;
+        // console.log('Counter has been reset to ' + countValue + '.')
     }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh);
     countContainer.textContent = countValue;
 }
 main()
